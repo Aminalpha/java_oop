914fc8c8876ced2359379fbebcacfa6fe0a98916
Amina <ba.amina.01@gmail.com>
2024-08-21 22:29:07 -0400

    Initialize project

---
+++ src/main/java/com/myhouse/java_oop/JavaOopApplication.java
@@ -0,0 +1,13 @@
+package com.myhouse.java_oop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaOopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaOopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myhouse/java_oop/JavaOopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myhouse.java_oop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaOopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

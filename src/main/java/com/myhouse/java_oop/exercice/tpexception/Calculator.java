0f30750388a26703632f133c3555f950f59818bd
Amina <ba.amina.01@gmail.com>
2024-10-16 00:52:55 -0400

    Improve Calculator

--- src/main/java/com/myhouse/java_oop/exercice/tpexception/Calculator.java
+++ src/main/java/com/myhouse/java_oop/exercice/tpexception/Calculator.java
@@ -5,57 +5,63 @@ import java.util.Scanner;
 
 public class Calculator {
 
-    public void divide(int a, int b){
+    Scanner scan =  new Scanner(System.in);
 
-        try {
-            float result = a / b;
-            System.out.println("Le resultat de la division de "+a+" par "+b+" est: "+result);
-        } catch (ArithmeticException ae) {
-            System.out.println("Division par 0 n'existe pas");
+    public float divide(int a, int b){
+        return a / b;
+    }
+
+    public void calculatrice(int a, int b){
+        System.out.print("Enter operation (+, -, *, /): ");
+        char operation = scan.next().charAt(0);
+        switch (operation) {
+            case '+':
+                System.out.println("Le resultat de l'addition est: "+ (a + b));
+                break;
+            case '-':
+                System.out.println("Le resultat de la soustraction est: " + (a - b));
+                break;
+            case '*':
+                System.out.println("Le resultat de la multiplication est: " + a * b);
+                break;
+            case '/':
+                try {
+                    System.out.println("Le resultat de la division de "+a+" par "+b+" est: "+divide(a,b));
+                } catch (ArithmeticException ae) {
+                    System.out.println("Division par 0 n'existe pas");
+                }
+                break;
+            default:
+                System.out.println("L'opérateur n'existe pas");
+
         }
     }
 
+    public void saisiEntier(){
+        try {
+            System.out.println("Entrer le premier nombre: ");
+
+            int a = scan.nextInt();
+            System.out.println("Entrer le deuxième nombre: ");
+            int b = scan.nextInt();
+
+            calculatrice(a, b);
+
+        } catch (InputMismatchException ie) {
+            System.out.println("Vous devez saisir un entier");
+            scan.next();
+        }
+    }
     public void getInput(){
         int exit = 0;
-        Scanner scanner = new Scanner(System.in);
         while(exit == 0) {
-            try {
-                System.out.println("Entrer le premier nombre: ");
-
-                int a = scanner.nextInt();
-                System.out.println("Entrer le deuxième nombre: ");
-                int b = scanner.nextInt();
-                System.out.print("Enter operation (+, -, *, /): ");
-                char operation = scanner.next().charAt(0);
-
-                switch (operation) {
-                    case '+':
-                        System.out.println("Le resultat de l'addition est: "+ (a + b));
-                        break;
-                    case '-':
-                        System.out.println("Le resultat de la soustraction est: " + (a - b));
-                        break;
-                    case '*':
-                        System.out.println("Le resultat de la multiplication est: " + a * b);
-                        break;
-                    case '/':
-                        divide(a, b);
-                        break;
-                    default:
-                        System.out.println("L'opérateur n'existe pas");
-
-                }
-
-            } catch (InputMismatchException ie) {
-                System.out.println("Vous devez saisir un entier");
-                scanner.next();
-            }
+            saisiEntier();
             System.out.print("Tapez 0 pour continuer : ");
-            if (scanner.hasNextInt())
-                exit = scanner.nextInt();
+            if (scan.hasNextInt())
+                exit = scan.nextInt();
             else {
                 System.out.print("Entier non saisi : ");
-                scanner.next();
+                scan.next();
             }
         }
     }
@@ -64,7 +70,6 @@ public class Calculator {
     public static void main(String[] args){
 
         Calculator calculator = new Calculator();
-        //calculator.divide(10, 0);
         calculator.getInput();
     }
 
